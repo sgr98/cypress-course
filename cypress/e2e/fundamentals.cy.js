5ae948a3368f19a2d55a607524023bcd05b0d0f7
sgr98 <60283755+sgr98@users.noreply.github.com>
2024-05-20 19:26:05 +0530

    test(Fundamentals): Added first test to Fundamentals section

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,21 @@
 describe('Fundamentals Test', () => {
-	it('passes', () => {
-		cy.visit('http://localhost:3000/fundamentals');
+	beforeEach(() => {
+        cy.visit('/fundamentals')
+    })
+
+    it('Contains correct header text', () => {
+		// cy.get('[data-test="fundamentals-header"]').contains(/testing fundamentals/i)
+		cy.get('[data-test="fundamentals-header"]').should(
+			'contain.text',
+			'Testing Fundamentals'
+		);
+	});
+
+	it.only('Accordion works correctly', () => {
+        cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+        cy.get('[data-test="accordion-item-1"] div[role="button"]').click()    // can add .pause() to pause the test runner
+        cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+        cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+        cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
 	});
 });
