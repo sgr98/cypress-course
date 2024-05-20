d7e020fdff2f0a2c218414e941fa5460405404c2
sgr98 <60283755+sgr98@users.noreply.github.com>
2024-05-20 23:20:03 +0530

    test(post-button): Added intercepts to testing

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,42 +1,38 @@
 describe('Various examples', () => {
 	beforeEach(() => {
-		cy.visit('/');
+		cy.visit('/examples');
 	});
 
 	it('multi-page testing', () => {
+        cy.visit('/');
+
 		cy.getDataTest('nav-why-cypress').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/');
-		cy.wait(100);
 
 		cy.getDataTest('nav-overview').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/overview');
-		cy.wait(100);
 
 		cy.getDataTest('nav-fundamentals').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/fundamentals');
-		cy.wait(100);
 
 		cy.getDataTest('nav-forms').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/forms');
-		cy.wait(100);
 
 		cy.getDataTest('nav-examples').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/examples');
-		cy.wait(100);
 
 		cy.getDataTest('nav-component').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/component');
-		cy.wait(100);
 
 		cy.getDataTest('nav-practices').click();
-		cy.wait(100);
 		cy.location('pathname').should('equal', '/best-practices');
-		cy.wait(100);
+	});
+
+	it.only('intercepts testing', () => {
+		cy.intercept('POST', 'http://localhost:3000/examples', {
+			fixture: 'example.json',
+		});
+
+		cy.getDataTest('post-button').click();
 	});
 });
